0ddab5620fa437bfc89d3d0d2f4687ace8b3b39f
Ha Huy Hieu <16.huyhieu.10a7@gmail.com>
2026-07-16 00:08:34 +0700

    Hoan thanh Lab1

---
+++ Lab1_bai1.c
@@ -0,0 +1,10 @@
+// Ham chinh khai bao thu vien
+#include <stdio.h>
+// Ham chinh de chay chuong trinh
+int main() {
+    // In ra mang hinh chuoi
+    printf("My Name : Ha Huy Hieu");
+    printf("\nLop : SD22202_COM108 ");  
+
+    return 0;
+}
